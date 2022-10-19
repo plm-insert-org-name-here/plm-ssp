93a1a951612e370b6a43aa6bc980876fbabb1390
Bór Milán <milanbor40@gmail.com>
2022-10-19 13:42:03 +0200

    opu GetById

--- backend/Api/Endpoints/OPUs/GetById.cs
+++ backend/Api/Endpoints/OPUs/GetById.cs
@@ -1,6 +1,53 @@
+using Domain.Entities.CompanyHierarchy;
+using FastEndpoints;
+using Infrastructure.Database;
+
 namespace Api.Endpoints.OPUs;
 
-public class GetById
+public class GetById : Endpoint<GetById.Req, GetById.Res>
 {
-    
+    public IRepository<OPU> OPURepo { get; set; } = default!;
+    public class Req
+    {
+        public int Id { get; set; }
+    }
+
+    public class Res
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        // public List<Line> Lines { get; set; } = default!;
+        // public Site Site { get; set; } = default!;
+        // public int SiteId { get; set; }
+    }
+
+    private static Res MapOut(OPU o) => new()
+    {
+        Id = o.Id,
+        Name = o.Name,
+        // Lines = o.Lines,
+        // Site = o.Site,
+        // SiteId = o.SiteId
+    };
+
+    public override void Configure()
+    {
+        Get(Api.Routes.OPUs.GetById);
+        AllowAnonymous();
+        Options(x => x.WithTags("OPUs"));
+    }
+
+    public override async Task HandleAsync(Req req, CancellationToken ct)
+    {
+        var opu = await OPURepo.GetByIdAsync(req.Id, ct);
+
+        if (opu is null)
+        {
+            await SendNotFoundAsync();
+            return;
+        }
+
+        var res = MapOut(opu);
+        await SendOkAsync(res, ct);
+    }
 }
