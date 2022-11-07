dab601a66c6fcac1c37f8a90cfc74828e3412074
Bór Milán <milanbor40@gmail.com>
2022-11-07 12:04:37 +0100

    csak az aktív taskinstance jön le

--- backend/Api/Endpoints/Tasks/GetInstance.cs
+++ backend/Api/Endpoints/Tasks/GetInstance.cs
@@ -33,7 +33,7 @@ public class GetInstance : Endpoint<GetInstance.Req, GetInstance.Res>
 
     public override async System.Threading.Tasks.Task HandleAsync(Req req, CancellationToken ct)
     {
-        var instance = await InstanceRepo.FirstOrDefaultAsync(new TaskInstanceWithEvents(req.TaskId), ct);
+        var instance = await InstanceRepo.FirstOrDefaultAsync(new ActiveTaskInstanceWithEvents(req.TaskId), ct);
 
         if (instance is null)
         {
